bf5ff2412c9e7a2f3b255d477d24b367973d7717
LikhitaSree52 <121919778+LikhitaSree52@users.noreply.github.com>
2023-10-18 12:22:05 +0530

    Add files via upload

---
+++ DLL_count.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+struct node
+{
+	struct node* prev_link;
+	int data;
+	struct node* next_link;
+};
+void display(struct node* head);
+int main()
+{
+	struct node* head=(struct node*)malloc(sizeof(struct node));
+	head->prev_link=NULL;
+	head->data=10;
+	head->next_link=NULL;
+	struct node* current=(struct node*)malloc(sizeof(struct node));
+	current->prev_link=NULL;
+	current->data=20;
+	current->next_link=NULL;
+	head->next_link=current;
+	current->prev_link=head;
+	display(head);
+}
+void display(struct node* head)
+{
+	struct node* temp=head;
+	int count=0;
+	if(head==NULL)
+		printf("List is empty");
+	else
+	{
+		while(temp!=NULL)
+		{
+			count++;
+			temp=temp->next_link;
+		}
+		printf("Count:%d \n",count);
+	}
+}
+
